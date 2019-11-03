5461d8b233f68e9ccce945e098815a9c631afc41
wasteee <fghj82110@gmail.com>
2019-11-03 18:43:37 +0800

    Add files via upload

---
+++ svr_9class.py
@@ -0,0 +1,218 @@
+"""The template of the main script of the machine learning process
+"""
+import pickle
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import csv
+
+dirpath = 'C:\\Users\\fghj8\\MLGame-master\\games\\arkanoid\\log\\10'
+BallPosition = []
+PlatformPosition = []
+#DL = 1 , DR = 2 , UL = 3 , UR = 4
+LRUP = []
+Slope = []
+last_ball_x = 0
+last_ball_y = 0
+files = listdir(dirpath)
+log_number = 0
+Frame = []
+bricks = []
+hit = False
+hitarray = []
+Plat_X = []
+PlatX = []
+ball_to_200 = 0
+ballx_to_platmid = 0
+bally_to_platmid = 0
+ball_to_plat = []
+zerocount = 0
+np.set_printoptions(threshold=np.inf)
+
+for k in range(0,1):
+    for f in files:
+      log_number = log_number + 1
+      fullpath = join(dirpath, f)
+      if isfile(fullpath):
+        with open(fullpath , "rb") as f1:
+            data_list1 = pickle.load(f1)
+        for i in range(0 , len(data_list1)):
+            table = np.array(np.zeros((208,408)))
+            bricks = data_list1[i].bricks
+            for k in range(len(bricks)):
+                for p in range(0,25):
+                    for j in range(0,10):
+                        table[bricks[k][0] + p][bricks[k][1] + j ] = 1
+            BallPosition.append(data_list1[i].ball)
+              
+            PlatformPosition.append(data_list1[i].platform)
+            Frame.append(data_list1[i].frame)
+            if(last_ball_x - data_list1[i].ball[0] > 0):
+                if(last_ball_y - data_list1[i].ball[1] > 0):
+                    #going up
+                    ball_to_200 = 200 - int(data_list1[i].ball[0])
+                    LRUP.append(np.array((3,ball_to_200)))
+                    ballx_to_platmid = np.abs((data_list1[i].ball[0]-data_list1[i].platform[0]))
+                    ball_to_plat.append(np.array((ballx_to_platmid,400-data_list1[i].ball[1])))
+                    #U.L
+                    j = data_list1[i].ball[0]
+                    k = data_list1[i].ball[1]
+                    while(j > 0 and k > 0):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j-1
+                        k = k-1
+                        hit = False
+                    if(hit==False):
+                        j = data_list1[i].ball[0]
+                        k = data_list1[i].ball[1]+3
+                        while(j > 0 and k > 0):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j-1
+                            k = k-1
+                            hit = False
+                    
+                    hitarray.append(np.array((j,k)))
+                    
+                else:
+                    #going down
+                    ball_to_200 = 200 - int(data_list1[i].ball[0])
+                    LRUP.append(np.array((1,ball_to_200)))
+                    ballx_to_platmid = np.abs((data_list1[i].ball[0]-data_list1[i].platform[0]))
+                    ball_to_plat.append(np.array((ballx_to_platmid,400-data_list1[i].ball[1])))
+                    #DL
+                    j = data_list1[i].ball[0]
+                    k = data_list1[i].ball[1]
+                    while(j > 0 and k < 400):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j-1
+                        k = k+1
+                        hit = False
+                    if(hit==False):
+                        j = data_list1[i].ball[0]
+                        k = data_list1[i].ball[1]+3
+                        while(j > 0 and k < 400):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j-1
+                            k = k+1
+                            hit = False
+                    hitarray.append(np.array((j,k)))
+                    
+            else:
+                if(last_ball_y - data_list1[i].ball[1] > 0):
+                    #going up
+                    ball_to_200 = 200 - int(data_list1[i].ball[0])
+                    LRUP.append(np.array((4,ball_to_200)))
+                    ballx_to_platmid = np.abs((data_list1[i].ball[0]-data_list1[i].platform[0]))
+                    ball_to_plat.append(np.array((ballx_to_platmid,400-data_list1[i].ball[1])))
+                    #U.R
+                    j = data_list1[i].ball[0]+3
+                    k = data_list1[i].ball[1]
+                    while(j < 200 and k > 0):
+                        
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j+1
+                        k = k-1
+                        hit = False
+                    if(hit==False):
+                        j = data_list1[i].ball[0]+3
+                        k = data_list1[i].ball[1]+3
+                        while(j < 200 and k > 0):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j+1
+                            k = k-1
+                            hit = False
+                    hitarray.append(np.array((j,k)))
+                else:
+                    #going down
+                    ball_to_200 = 200 - int(data_list1[i].ball[0])
+                    LRUP.append(np.array((2,ball_to_200)))
+                    ballx_to_platmid = np.abs((data_list1[i].ball[0]-data_list1[i].platform[0]))
+                    ball_to_plat.append(np.array((ballx_to_platmid,400-data_list1[i].ball[1])))
+                    #D.R.
+                    j = data_list1[i].ball[0]+3
+                    k = data_list1[i].ball[1]
+                    while(j < 200 and k < 400):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j+1
+                        k = k+1
+                        hit = False
+                    if(hit==False):
+                        j = data_list1[i].ball[0]+3
+                        k = data_list1[i].ball[1]+3
+                        while(j < 200 and k < 400):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j+1
+                            k = k+1
+                            hit = False
+                    hitarray.append(np.array((j,k)))
+            
+
+            last_ball_x = data_list1[i].ball[0]
+            last_ball_y = data_list1[i].ball[1]
+PlatX = np.array(PlatformPosition) [:,0][:,np.newaxis]
+PlatX_next = PlatX[1:,:]
+instrust = (PlatX_next-PlatX[0:len(PlatX_next),0][:,np.newaxis])/5
+Plat_X = PlatX
+for i in range(0,len(Plat_X)):
+    Plat_X[i][0] = Plat_X[i][0] +20
+
+
+ball_to_plat =  np.array(ball_to_plat[:-1])
+Ballarray = np.array(BallPosition[:-1])
+hitarray = np.array((hitarray[:-1]))
+LRUP = np.array((LRUP[:-1]))
+x = np.hstack((Ballarray,hitarray,LRUP,ball_to_plat,Plat_X[0:-1,0][:,np.newaxis]))
+
+#PlatX = np.array(PlatX)
+#new = np.hstack((x,PlatX[:,np.newaxis]))
+
+
+y = instrust 
+
+#with open('C:\\Users\\fghj8\\MLGame-master\\svr.csv', 'w', newline='') as csvfile:
+#    writer = csv.writer(csvfile)
+#    writer.writerow(['ball_x','ball_y','hit_x','hit_y','platform_x','next_x'])
+#    writer.writerows(new)
+    
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_state = 41)
+
+from sklearn.svm import SVR
+
+svr = SVR(gamma=0.001,C = 1,epsilon = 0.1,kernel = 'rbf')
+
+
+svr.fit(x_train,y_train)   
+y_predict = svr.predict(x_test)
+
+from sklearn.metrics import r2_score#R square
+    
+R2 = r2_score(y_test,y_predict)
+print('R2 = ',R2)
+print(len(Frame))
+print('log number : ' + str(log_number))
+
+
+filename = "C:\\Users\\fghj8\\MLGame-master\\games\\arkanoid\\svr_9class.sav"
+pickle.dump(svr,open(filename,"wb"))
+
+
+            
+
---
+++ svr_play_9c.py
@@ -0,0 +1,208 @@
+"""The template of the main script of the machine learning process
+"""
+import pickle
+import games.arkanoid.communication as comm
+import numpy as np
+from games.arkanoid.communication import ( \
+    SceneInfo, GameInstruction, GameStatus, PlatformAction
+)
+
+def ml_loop():
+    """The main loop of the machine learning process
+
+    This loop is run in a separate process, and communicates with the game process.
+
+    Note that the game process won't wait for the ml process to generate the
+    GameInstruction. It is possible that the frame of the GameInstruction
+    is behind of the current frame in the game process. Try to decrease the fps
+    to avoid this situation.
+    """
+
+    # === Here is the execution order of the loop === #
+    # 1. Put the initialization code here.
+
+    # 2. Inform the game process that ml process is ready before start the loop.
+    filename = "C:\\Users\\fghj8\\MLGame-master\\games\\arkanoid\\svr_9class.sav"
+    model = pickle.load(open(filename,'rb'))
+
+    last_ball_x = 0
+    last_ball_y = 0
+    games = 0
+    gameover = 0
+    inputx = 0
+    inputy = 0
+    hit = False
+    LRUP = 0
+    ball_to_200 = 0
+    comm.ml_ready()
+    bricks = []
+    # 3. Start an endless loop.
+    scene_info = comm.get_scene_info()
+    while True:
+        # 3.1. Receive the scene information sent from the game process.
+        
+    
+        table = np.array(np.zeros((208,408)))
+        
+        last_ball_x = scene_info.ball[0]
+        last_ball_y = scene_info.ball[1]
+        
+        scene_info = comm.get_scene_info()
+        bricks = scene_info.bricks
+        for k in range(len(bricks)):
+            for p in range(0,25):
+                for j in range(0,10):
+                    table[bricks[k][0] + p][bricks[k][1] + j ] = 1
+        
+        if(last_ball_x - scene_info.ball[0] > 0):
+           
+                if(last_ball_y - scene_info.ball[1] > 0):
+                    #going up
+                    inputx = scene_info.ball[0]
+                    inputy = scene_info.ball[1]
+                    LRUP = 3
+                    #U.L
+                    j = scene_info.ball[0]
+                    k = scene_info.ball[1]
+                    while(j > 0 and k > 0):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j-1
+                        k = k-1
+                        hit = False
+                    if(hit==False):
+                        j = scene_info.ball[0]
+                        k = scene_info.ball[1]+3
+                        while(j > 0 and k > 0):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j-1
+                            k = k-1
+                            hit = False
+                        
+                  
+                else:
+                    #going down
+                    #DL
+                    LRUP = 1
+                    inputx = scene_info.ball[0]
+                    inputy = scene_info.ball[1]
+                    j = scene_info.ball[0]
+                    k = scene_info.ball[1]
+                    while(j > 0 and k < 400):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j-1
+                        k = k+1
+                        hit = False
+                    if(hit==False):
+                        j = scene_info.ball[0]
+                        k = scene_info.ball[1]+3
+                        while(j > 0 and k < 400):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j-1
+                            k = k+1
+                            hit = False
+                   
+                    
+        else:
+                
+                if(last_ball_y - scene_info.ball[1] > 0):
+                    #going up
+                    LRUP = 4
+                    inputx = scene_info.ball[0]+3
+                    inputy = scene_info.ball[1]
+                    #U.R
+                    j = scene_info.ball[0]
+                    k = scene_info.ball[1]
+                    while(j < 200 and k > 0):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j+1
+                        k = k-1
+                        hit = False
+                    if(hit==False):
+                        j = scene_info.ball[0]+3
+                        k = scene_info.ball[1]+3
+                        while(j < 200 and k > 0):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j+1
+                            k = k-1
+                            hit = False
+                   
+                else:
+                    #going down
+                    inputx = scene_info.ball[0]+3
+                    inputy = scene_info.ball[1]
+                    LRUP = 2
+                    #D.R.
+                    j =scene_info.ball[0]+3
+                    k = scene_info.ball[1]
+                    while(j < 200 and k < 400):
+                        if(table[j][k] == 1):
+                            hit = True
+                            break
+                        j = j+1
+                        k = k+1
+                        hit = False
+                    if(hit==False):
+                        j = scene_info.ball[0]+3
+                        k = scene_info.ball[1]+3
+                        while(j < 200 and k < 400):
+                            if(table[j][k] == 1):
+                                hit = True
+                                break
+                            j = j+1
+                            k = k+1
+                            hit = False
+                    
+                    
+        ballx_to_platmid = np.abs((scene_info.ball[0]-scene_info.platform[0]))
+        bally_to_platmid = 400-scene_info.ball[1]
+#        print(j,k)
+        inp_temp = np.array([inputx,inputy,j,k,LRUP,(200 - int(scene_info.ball[0])),ballx_to_platmid,bally_to_platmid,scene_info.platform[0]+20])
+        input = inp_temp[np.newaxis,:]
+        
+        if scene_info.status == GameStatus.GAME_OVER or \
+            scene_info.status == GameStatus.GAME_PASS:
+            games = games + 1
+#            print(games)
+            if(scene_info.status == GameStatus.GAME_OVER):
+#                print('game over')
+#                gameover = gameover + 1
+#                print((games - gameover) / games)
+                 pass
+            # Do some stuff if needed
+            # 3.2.1. Inform the game process that ml process is ready
+            comm.ml_ready()
+            continue
+        
+            
+        
+        
+        move = model.predict(np.around(input))
+#        next_x = move * 20
+#        next_x = next_x - next_x%5
+#        print(move)
+    
+        # 3.3. Put the code here to handle the scene information
+
+        # 3.4. Send the instruction for this frame to the game process
+        
+        if(move < -0.1):
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+        elif(move > 0.1):
+            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+        else:
+            comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+    
+            
+
